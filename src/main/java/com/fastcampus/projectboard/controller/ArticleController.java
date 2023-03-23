4121ab5c98968cf6fde69f64f473d4feef41cb75
imsadboy <wos961129@gmail.com>
2023-03-23 19:56:15 +0900

    #20 - 컨트롤러에 오타및 누락내용
    ㅠㅠ PropertyReferenceException 이 계속 떠서 확인해보니 내용에 오타 및 누락이 좀 있었음 수정사항 커밋

--- src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,18 +26,21 @@ public class ArticleController {
     public String articles(
             @RequestParam(required = false) SearchType searchType,
             @RequestParam(required = false) String searchValue,
-            @PageableDefault(size = 10, sort = "createAt", direction = Sort.Direction.DESC) Pageable pageable,
-            Model map
-    ){
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+    ) {
         map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
-    public String article(@PathVariable Long articleId, Model map){
+    public String article(@PathVariable Long articleId, ModelMap map) {
         ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
-        map.addAttribute("article",article);
+        map.addAttribute("article", article);
         map.addAttribute("articleComments", article.articleCommentsResponse());
+
         return "articles/detail";
     }
+
 }
